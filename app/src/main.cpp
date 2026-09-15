27d86bb700aed2213d3872cb5f8708913d3679a2
Franco <frdr3076@gmail.com>
2026-09-14 22:23:30 -0300

    added asignment devicetree l4-task1

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -2,11 +2,31 @@
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 
-
-
+/*Commands:
+    Build: west build -b frdm_mcxa156 app -p
+    Flash: west flash
+    menuconfig: west build -t menuconfig
+    zephyr.dts is found here: .\zephyr-course\build\zephyr\zephyr.dts
+*/
 
 /* The devicetree node identifier for the "led0" alias. */
-#define LED_NODE DT_ALIAS(led0)
+
+//#define LED_NODE DT_ALIAS(warning_led)
+#define LED_NODE DT_ALIAS(app_led)
+
+    /* Method 1: using nodelabel
+    //#define LED_NODE DT_NODELABEL(red_led)
+    */
+
+    /* Method 2: ussing path
+    //#define LED_NODE DT_PATH(leds,led_1)
+    */
+
+    /* Method 3: using other overlays
+    //west build -- -DEXTRA_DTC_OVERLAY_FILE="boards/green.overlay"
+    */
+
+//Not used: //#define LED_NODE DT_ALIAS(led0)
 
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED_NODE, gpios);
 
@@ -25,7 +45,11 @@ int main(void)
 
         led_state = !led_state;
         LOG_INF("LED state: %s", led_state ? "ON" : "OFF");
-        k_msleep(CONFIG_BLINK_SLEEP_TIME_MS);
+
+
+        //k_msleep(CONFIG_BLINK_SLEEP_TIME_MS);    // Use for Module 01 to 03
+        k_msleep(CONFIG_APP_HEARTBEAT_PERIOD_MS); // Use for Module 04
+
     }
     return 0;
 }
